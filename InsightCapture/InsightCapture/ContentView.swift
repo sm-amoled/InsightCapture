5ad224fccf261b02fe743b1913822d80fc029418
sm-amoled <moktak072@naver.com>
2023-03-07 16:01:30 +0900

    🎉 프로젝트 시작!

---
+++ InsightCapture/InsightCapture/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  InsightCapture
+//
+//  Created by Park Sungmin on 2023/03/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InsightCapture/InsightCapture/InsightCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsightCaptureApp.swift
+//  InsightCapture
+//
+//  Created by Park Sungmin on 2023/03/07.
+//
+
+import SwiftUI
+
+@main
+struct InsightCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
