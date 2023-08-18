a5dffd00affd7659768f4f1f531292cc584911ae
sm-amoled <moktak072@naver.com>
2023-08-18 18:30:34 +0900

    ✨[#23] MyPageView 레이아웃 작성

--- InsightCapture/InsightCapture/MyPageView/MyPageView.swift
+++ InsightCapture/InsightCapture/MyPageView/MyPageView.swift
@@ -8,8 +8,134 @@
 import SwiftUI
 
 struct MyPageView: View {
+    @Environment(\.dismiss) var dismiss
+
     var body: some View {
-        Text("My Page")
+        ZStack(alignment: .top) {
+            Rectangle()
+                .foregroundColor(Color(uiColor: UIColor.systemGray5))
+//                .foregroundColor(Color.red)
+                .edgesIgnoringSafeArea(.vertical)
+            
+            VStack {
+                Spacer()
+                    .frame(height: 50)
+                
+                List {
+                    Section("나의 기록") {
+                        VStack(alignment: .leading, spacing: 16) {
+                            HStack(spacing: 0){
+                                Text("📅 ")
+                                Text("생각을 함께 수집한 지 ")
+                                Text("5개월")
+                                    .font(Font.system(size: 17, weight: .bold))
+                                Text("이 되었어요.")
+                            }
+                            .font(Font.system(size: 15, weight: .regular))
+                            
+                            HStack(spacing: 0){
+                                Text("🕊️ ")
+                                Text("그 동안 생각, 날 것을 ")
+                                Text("127번")
+                                    .font(Font.system(size: 17, weight: .bold))
+                                Text(" 실행했어요.")
+                            }
+                            .font(Font.system(size: 15, weight: .regular))
+                            
+                            HStack(spacing: 0){
+                                Text("👀 ")
+                                Text("벌써 ")
+                                Text("30개")
+                                    .font(Font.system(size: 17, weight: .bold))
+                                Text("의 영감을 기록했어요!")
+                            }
+                            .font(Font.system(size: 15, weight: .regular))
+                            
+                            HStack(spacing: 0){
+                                Text("🏠 ")
+                                Text("주로 영감을 얻은 곳은 ")
+                                Text("YouTube")
+                                    .font(Font.system(size: 17, weight: .bold))
+                                Text(" 였어요.")
+                            }
+                            .font(Font.system(size: 15, weight: .regular))
+                        }
+                        .padding(.vertical, 16)
+                    }
+                    .listRowSeparator(.hidden)
+                    .font(Font.system(size: 18, weight: .semibold))
+                    .foregroundColor(.black)
+                    
+                    Section("이용 안내") {
+                        NavigationLink("공지사항을 알려드립니다") {
+                            return EmptyView()
+                        }
+                        .font(Font.system(size: 15, weight: .regular))
+                        .foregroundColor(.black)
+                        
+                        NavigationLink("문의는 여기로 남겨주세요") {
+                            return EmptyView()
+                        }
+                        .font(Font.system(size: 15, weight: .regular))
+                        .foregroundColor(.black)
+                    }
+                    .font(Font.system(size: 18, weight: .semibold))
+                    .foregroundColor(.black)
+                    
+                    Section("바라는 점이 있나요") {
+                        NavigationLink("생각, 날 것이 더 유용해지기 위해\n필요한 기능들을 제안해주세요!") {
+                            return EmptyView()
+                        }
+                        .padding(.vertical, 10)
+                        .lineSpacing(8)
+                        .font(Font.system(size: 15, weight: .regular))
+                        .foregroundColor(.black)
+                    }
+                    .listRowSeparator(.hidden)
+                    .font(Font.system(size: 18, weight: .semibold))
+                    .foregroundColor(.black)
+                    
+                    Section("약관 및 정책") {
+                        NavigationLink("이용 약관") {
+                            return EmptyView()
+                        }
+                        .font(Font.system(size: 15, weight: .regular))
+                        .foregroundColor(.black)
+                        
+                        NavigationLink("개인정보 처리 방침") {
+                            return EmptyView()
+                        }
+                        .font(Font.system(size: 15, weight: .regular))
+                        .foregroundColor(.black)
+                    }
+                    .font(Font.system(size: 18, weight: .semibold))
+                    .foregroundColor(.black)
+                }
+                .scrollContentBackground(.hidden)
+                .background(Color(uiColor: UIColor.systemGray5))
+                .scrollIndicators(.hidden)
+            }
+            
+            
+            HStack(spacing: 8) {
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "chevron.left")
+                        .font(Font.system(size: 18, weight: .semibold))
+                        .foregroundColor(.black)
+                }
+                
+                Text("내 정보")
+                    .font(Font.system(size: 18, weight: .semibold))
+                
+                Spacer()
+            }
+            .padding(.horizontal, 16)
+            .padding(.vertical, 12)
+        }
+        .navigationBarBackButtonHidden(true)
+        .toolbar(.hidden, for: .navigationBar)
     }
 }
 
